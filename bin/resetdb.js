631ae3350efcb86563df92c591c0a67cf15bc6a5
lisafaggiolo <lisa.faggiolo@live.ca>
2020-09-18 19:54:59 +0000

    database connection made.

--- bin/resetdb.js
+++ bin/resetdb.js
@@ -8,7 +8,7 @@ const Client = require('pg-native');
 
 // PG connection setup
 const connectionString = process.env.DATABASE_URL ||
-  `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+  `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=disable`;
 const client = new Client();
 
 // Loads the schema files from db/schema
@@ -43,6 +43,6 @@ try {
 } catch (err) {
   console.error(chalk.red(`Failed due to error: ${err}`));
   client.end();
-}
+};
 
 
