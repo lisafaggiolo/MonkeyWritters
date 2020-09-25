a6403b28033f355bba6bca59c1d378637715ec19
Lisa <65428933+lisafaggiolo@users.noreply.github.com>
2020-09-25 15:03:21 -0400

    Last min stuff (#8)
    * last minute fixes
    
    * final cleanup (could definitely be cleaner)

--- routes/stories.js
+++ routes/stories.js
@@ -9,7 +9,6 @@ const express = require('express');
 const router  = express.Router();
 const dbHelpers = require('./helpers/dbHelpers.js');
 const cookieSession = require("cookie-session");
-const { text } = require('body-parser');
 const app        = express();
 
 app.use(cookieSession({
@@ -20,6 +19,7 @@ app.use(cookieSession({
 
 
 module.exports = (db) => {
+  //GETS SELECTED STORY TO ADD A CONTRIBUTION TO
   router.get("/", (req, res) => {
     db.query(`select stories.*,users.username as owner, users.avatar_url from stories join users on (users.id = stories.user_id);`)
       .then(data => {
@@ -36,7 +36,7 @@ module.exports = (db) => {
   });
 
 
-
+  //ADDS CONTRIBUTION FOR A SPECIFIC STORY
   router.post("/:storyID/prospects", (req, res) => {
    // console.log("STORYID/prospects")
     const user_id = req.session.user_id;
@@ -65,44 +65,48 @@ module.exports = (db) => {
     });
   });
 
+  /** We were so close but ultimately couldnt make the story merging button/functionality work**/
 
-  router.post("/:storyID/prospects/:prospectsID", (req, res) => {
-    const user_id = req.session.user_id;
-    const storyID = req.params.storyID;
-    const prospectsID = req.params.prospectsID
+
+  // router.post("/:storyID/prospects/:prospectsID", (req, res) => {
+  //   const user_id = req.session.user_id;
+  //   const storyID = req.params.storyID;
+  //   const prospectsID = req.params.prospectsID
     
-    console.log("USER ID",user_id)
-    console.log("STORY ID", storyID)
-    console.log("PROSPECT ID", prospectsID)
+  //   console.log("USER ID",user_id)
+  //   console.log("STORY ID", storyID)
+  //   console.log("PROSPECT ID", prospectsID)
 
 
 
-    db.query(`SELECT prospects.content
-    FROM prospects 
-    WHERE prospects.id = ${prospectsID};`)
-    .then(data => {
-      const contribution = data.rows[0].content;
-      console.log("CONTRIBUTION =>",contribution);
-      db.query(`UPDATE stories
-      SET prospect = "Hello there"
-      WHERE id = ${storyID};`)
-      .then(() => {
-        res.redirect(`/stories/${storyID}`);
-      })
+  //   db.query(`SELECT prospects.content
+  //   FROM prospects 
+  //   WHERE prospects.id = ${prospectsID};`)
+  //   .then(data => {
+  //     const contribution = data.rows[0].content;
+  //     console.log("CONTRIBUTION =>",contribution);
+  //     db.query(`UPDATE stories
+  //     SET prospect = "Hello there"
+  //     WHERE id = ${storyID};`)
+  //     .then(() => {
+  //       res.redirect(`/stories/${storyID}`);
+  //     })
       
   
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
-  });
+  //   })
+  //   .catch(err => {
+  //     res
+  //       .status(500)
+  //       .json({ error: err.message });
+  //   });
+  // });
 
+
+  //VOTING FUNCTIONNALITY ON CONTRIBUTIONS FROM MONKEYS
   router.post("/:storyID/prospects/:prospectID/vote", (req, res) => {
     const prospectID = req.params.prospectID;
     const storyID = req.params.storyID;
-    console.log(storyID);
+    //console.log(storyID);
     dbHelpers.addVote(prospectID)
     .then(() => {
       res.redirect(`/stories/${storyID}`);
@@ -117,28 +121,29 @@ module.exports = (db) => {
 
   
 
+  //GETS STORIES SPECIFIC TO USER
+  router.get("/mystories", (req, res) => {
+     //console.log("REQ.PARAMS ", req.params);
+     //console.log("REQ.BODY ", req.body);
+     //console.log("REQ.SESSION ", req.session);
 
-   router.get("/mystories", (req, res) => {
-     console.log("REQ.PARAMS ", req.params);
-     console.log("REQ.BODY ", req.body);
-     console.log("REQ.SESSION ", req.session);
-
-      db.query(`select stories.*,users.username as owner, users.avatar_url from stories join users on (users.id = stories.user_id) where user_id ='${req.session.user_id}';`)
-        .then(data => {
-          const stories = data.rows;
-          res.json({ stories });
-        })
-     .catch(err => {
-          res
-            .status(500)
-            .json({ error: err.message });
+    db.query(`select stories.*,users.username as owner, users.avatar_url from stories join users on (users.id = stories.user_id) where user_id ='${req.session.user_id}';`)
+      .then(data => {
+        const stories = data.rows;
+        res.json({ stories });
+      })
+      .catch(err => {
+        res
+        .status(500)
+        .json({ error: err.message });
 
         });
    });
 
 
+  //ADDS NEW STORY FOR A SPECIFIC USER 
   router.post("/mystories", (req, res) => {
-    console.log("mystories - POST")
+    //console.log("mystories - POST")
     const user_id = req.session.user_id;
     const { title, text } = req.body;
 
@@ -160,9 +165,11 @@ module.exports = (db) => {
 
   });
 
+
+  //GETS CONTRIBUTIONS SPECIFIC TO ONE STORY  
   router.get("/:storyID", (req, res) => {
-    console.log("STORYID")
-    console.log('req.params.storyID', req.params.storyID)
+    //console.log("STORYID")
+    //console.log('req.params.storyID', req.params.storyID)
 
     db.query(`SELECT prospects.*, users.username as username, users.avatar_url as avatar 
     FROM prospects 
