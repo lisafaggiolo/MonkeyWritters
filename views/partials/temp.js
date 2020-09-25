33312a978c7c5f36072c038de67e5fb4caee8a2c
Peteami <peteami@hotmail.com>
2020-09-25 01:13:46 +0000

    added button

---
+++ views/partials/temp.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.post("/storyID/prospects/:prospectsID", (req, res) => {
+    const user_id = req.session.user_id;
+    const storyID = req.params.storyID;
+    const prospectsID = req.params.prospectsID
+
+    db.query(SELECT * prospects where prospects.id = prospectsID)
+    .then(data => {
+
+      res.data.rows
+      UPDATE stories
+      SET content = CONCATE(content, data.content)
+      WHERE id = storyID;
+      update prospects set approved = 1 WHERE id = prospectsID
+      res.json({ stories });
+
+    })
+  });
+  return router;
